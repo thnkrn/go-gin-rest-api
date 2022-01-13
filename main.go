9797e5ca8f651082f706332cd1eb45682171328d
Thanakorn Ariyagusolsuthi <thanakorn.ari@truedigital.com>
2022-01-13 10:41:28 +0700

    feat: integrate gin

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	r := gin.New()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	r.Run()
 }
